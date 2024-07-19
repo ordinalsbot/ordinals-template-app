a9ef2c949f574ff69f7eb9c336286dfe5b61900b
Keegan-Lee <keegan.lee.francis@gmail.com>
2024-07-19 00:50:57 -0300

    chore: add more context to the homepag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,10 @@ export const metadata: Metadata = DEFAULT_METADATA;
 export default function Home() {
   const { width, height } = DEFAULT_DIMENSIONS;
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2 gap-3'>
       <h1 className='text-4xl font-bold mb-4'>Welcome to your <Image src={BTC} alt='btc-logo' width={width} height={height} className='inline' /> Ordinals NextJS App</h1>
       <p className='text-lg'>A template application with <Link className='hover:text-sky-500' href='https://www.npmjs.com/package/ordinalsbot' target='_blank'>ordinalsbot</Link>,  <Link className='hover:text-sky-500' href='https://docs.xverse.app/sats-connect' target='_blank'>sats-connect</Link>, and  <Link className='hover:text-sky-500' href='https://firebase.google.com/' target='_blank'>Firebase</Link></p>
+      <p className='text-md'>**Themed with <Link className='hover:text-sky-500' href='https://ui.shadcn.com/' target='_blank'>ShadCN</Link> and <Link className='hover:text-sky-500' href='https://tailwindcss.com/' target='_blank'>Tailwind</Link>**</p>
     </div>
   );
 };
